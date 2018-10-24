bd9a332ee0ffcf3014c3a0730aff9df4557e2832
wa2 <wa2@FSTU603027.pti.edu>
2018-10-24 16:37:41 -0400

    thtshithasio0tasdfkasdjfgaskdhf

--- src/classexercise4/ClassExercise4.java
+++ src/classexercise4/ClassExercise4.java
@@ -11,6 +11,7 @@ public class ClassExercise4
     public static void main(String[] args) 
     {
         System.out.println("Hi");
+        System.out.println("Kill Me!!!!!!!!!!!!!!!");
     }
     
 }
