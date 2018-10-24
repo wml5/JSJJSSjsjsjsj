5e25917889c71d421795ec696952a32d9c933578
wml5 <wml5@FSTU603026.pti.edu>
2018-10-24 16:34:48 -0400

    thtshithasio0t

---
+++ src/classexercise4/ClassExercise4.java
@@ -0,0 +1,16 @@
+/*
+ * William Ludwick
+ *10/24/2018
+ * Class Exercise 4
+ */
+package classexercise4;
+
+public class ClassExercise4 
+{
+
+    public static void main(String[] args) 
+    {
+        System.out.println("Hi");
+    }
+    
+}
